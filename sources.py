bd6fa190512a23ffd14c0c630de1dfda228a7fae
bart <106763622+Bartv0209@users.noreply.github.com>
2024-12-18 13:46:00 +0100

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,18 +1,3 @@
 news_sources = [
-    "https://www.bbc.com/", 
-    "https://www.dw.com/en/", 
-    "https://www.lemonde.fr/en/", 
-    "https://elpais.com/english/", 
-    "https://www.repubblica.it/english/", 
-    "https://www.asahi.com/ajw/", 
-    "https://www.hindustantimes.com/", 
-    "https://www.scmp.com/", 
-    "https://en.yna.co.kr/", 
-    "https://www.nytimes.com/", 
-    "https://www.washingtonpost.com/", 
-    "https://www.eluniversal.com.mx/english/", 
-    "https://www1.folha.uol.com.br/internacional/en/", 
-    "https://www.dailymaverick.co.za/", 
-    "https://www.theguardian.com/ng/"
-    "https://news.google.com/"
+#for furture expansion
 ]
